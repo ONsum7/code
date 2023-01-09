fc55ee882fccf379ba49a0d2452975c1ec290669
ONsum7 <116848959+ONsum7@users.noreply.github.com>
2023-01-09 13:22:15 +0000

    Update W7T1A.py

--- W7T1A.py
+++ W7T1A.py
@@ -13,4 +13,27 @@ print('')
 print('Marks have been saved')
 print('')
 
+with open('studentmark.txt', 'r') as f:
+  marks = f.readlines()
+
+marks = [int(mark) for mark in marks]
+
+average_mark = sum(marks) / len(marks)
+
+max_mark = max(marks)
+min_mark = min(marks)
+
+with open('studentmark.txt', 'a') as f:
+  f.write(f'\nAverage mark: {average_mark}\n')
+
+  f.write(f'Max mark: {max_mark}\n')
+  f.write(f'Min mark: {min_mark}\n')
+
+  for mark in marks:
+    if mark >= 40:
+      f.write(f'Mark {mark} is a pass\n')
+    else:
+      f.write(f'Mark {mark} is a fail\n')
+
+
 f.close()
